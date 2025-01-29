ad1f20f3be7688c4137d27cf5a6dcf0d51676a94
adiluexe <adiluexe@gmail.com>
2025-01-29 21:25:32 +0800

    Add style PracticeExercise4

--- PracticeExercise4/Input_Types_Output.php
+++ PracticeExercise4/Input_Types_Output.php
@@ -1,56 +1,76 @@
 <?php
-//Script name: Input_Types_Output.php
+// Script name: Input_Types_Output.php
 ?>
 
-<html> 
-	<head> 
-		<title> Use of HTML Form Tags in PHP </title> 
-	</head>
-<body bgcolor="99ffff">
-	<?php
-	
-	$name = $_POST['txtName'];
-	$age =  $_POST['txtAge'];
-	$course = $_POST['txtCourse'];
-	$rdo = $_POST['rdo'];
-	$job = $_POST['cboJob'];
-	$comments = $_POST['txtaComments'];
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Use of HTML Form Tags in PHP</title> 
+    <link rel="stylesheet" href="exercise4-style.css">
+</head>
+<body>
 
-	echo "OUTPUT From: <b> TextField </b> <br>";
-	echo "Hi " .$name. "<br>";
-	echo "You are ".$age." years old. <br>";
-	echo "Your course is " .$course. " <br><br>";
+    <div class="output-container">
+        <h2>Output From Your Input</h2>
 
-	echo "OUTPUT From: <b> Checkbox </b> <br>";
-	if (isset($_POST['chkYes']))
-		echo "Checkbox status: <b> ".$_POST['chkYes']."</b> <br><br>";
-	
-	echo "OUTPUT From: <b> Multiple CheckBoxes </b> <br>";
-	echo "Your favorite computer subject/s is/are: <br>";
-	if (isset($_POST['chkCP']))
-		echo "&nbsp&nbsp&nbsp " .$_POST['chkCP']. "<br>";
-	if (isset($_POST['chkCG']))
-		echo "&nbsp&nbsp&nbsp " .$_POST['chkCG']. "<br>";
-	if (isset($_POST['chkWBA']))
-		echo "&nbsp&nbsp&nbsp " .$_POST['chkWBA']. "<br><br>";
+        <?php
+        // Gather and display form input
+        $name = $_POST['txtName'];
+        $age = $_POST['txtAge'];
+        $course = $_POST['txtCourse'];
+        $rdo = $_POST['rdo'];
+        $job = $_POST['cboJob'];
+        $comments = $_POST['txtaComments'];
 
-	
-	echo "<br> OUTPUT From: <b> Radio Button </b> <br>";
-	if (isset($rdo))
-		echo "You selected: " .$rdo. "<br><br>";
-	
-	echo "OUTPUT From: <b> ComboBox </b> <br>";
-	echo "You prefered to be a: " .$job. " <br><br>";
+        echo "<div class='section'>
+                <b>TextField Output</b><br>
+                Hi, $name<br>
+                You are $age years old.<br>
+                Your course is $course.<br><br>
+              </div>";
 
-	echo "OUTPUT From: <b> ListBox </b> <br>";
-	echo "You prefered a thesis title like:<br>";
-	if (isset($_POST['lstThesisTitle']))
-		echo "&nbsp&nbsp&nbsp " .$_POST['lstThesisTitle']." <br><br>";
-	else
-		echo "NONE <br><br>";
+        echo "<div class='section'>
+                <b>Checkbox Output</b><br>";
+        if (isset($_POST['chkYes']))
+            echo "You are an officer of COMSOC.<br><br>";
+        echo "</div>";
 
-	echo "OUTPUT From: <b> TextArea </b> <br>";
-	echo $comments . " <br>";
-?>
+        echo "<div class='section'>
+                <b>Multiple Checkboxes Output</b><br>";
+        echo "Your favorite computer subject/s are:<br>";
+        if (isset($_POST['chkCP'])) echo "Computer Programming<br>";
+        if (isset($_POST['chkCG'])) echo "Computer Graphics<br>";
+        if (isset($_POST['chkWBA'])) echo "Web Based Application<br><br>";
+        echo "</div>";
+
+        echo "<div class='section'>
+                <b>Radio Button Output</b><br>
+                You prefer: $rdo<br><br>
+              </div>";
+
+        echo "<div class='section'>
+                <b>ComboBox Output</b><br>
+                You prefer to be a: $job<br><br>
+              </div>";
+
+        echo "<div class='section'>
+                <b>ListBox Output</b><br>
+                Your preferred thesis title(s) are:<br>";
+        if (isset($_POST['lstThesisTitle']))
+            echo implode(", ", $_POST['lstThesisTitle']);
+        else
+            echo "None<br><br>";
+        echo "</div>";
+
+        echo "<div class='section'>
+                <b>TextArea Output</b><br>
+                Your comments: $comments<br><br>
+              </div>";
+        ?>
+
+    </div>
+
 </body>
 </html>
