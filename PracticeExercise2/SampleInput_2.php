7e302853302f14bda89b6bd69f73bad0b966e9f0
adiluexe <adiluexe@gmail.com>
2025-01-29 21:15:05 +0800

    Add style PracticeExercise2

--- PracticeExercise2/SampleInput_2.php
+++ PracticeExercise2/SampleInput_2.php
@@ -1,18 +1,40 @@
 <?php
-//Sample Scripts
-//Script name: SampleInput_2.php
-	
-//Sample Input with validation
+// Sample Scripts
+// Script name: SampleInput_2.php
+// Sample Input with validation
 ?>
 
-DATA ENTRY <br><br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 2 - Data Entry</title>
+    <link rel="stylesheet" href="exercise2-style.css">
+</head>
+<body>
 
-<form action=SampleOutput_2.php method=post>
-Enter your name: <input type=text name=txtName><br>
-Enter your age: <input type=text name=txtAge><br>
-Enter your phone no.: <input type=text name=txtPhone><br>
-Enter your bill: <input type=text name=txtBill><br><br>	
+    <div class="form-container">
+        <h2>DATA ENTRY</h2>
+        <form action="SampleOutput_2.php" method="post">
+            <label>Enter your name:</label>
+            <input type="text" name="txtName" required>
 
-<input type=submit name=btnSubmit>
-<input type=reset>
-</form>
+            <label>Enter your age:</label>
+            <input type="text" name="txtAge" required>
+
+            <label>Enter your phone no.:</label>
+            <input type="text" name="txtPhone" required>
+
+            <label>Enter your bill:</label>
+            <input type="text" name="txtBill" required>
+
+            <div class="button-group">
+                <input type="submit" name="btnSubmit" value="Submit">
+                <input type="reset" value="Reset">
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
--- PracticeExercise2/SampleOutput_2.php
+++ PracticeExercise2/SampleOutput_2.php
@@ -1,33 +1,47 @@
 <?php
-//Sample Scripts
-//Script name: SampleOutput_2.php
-//Output from SampleInput_2.php
-	//if(ctype_alpha(str_replace(" ", "",$_POST['txtName']))) //Letters A-Z,a-z and white spaces are accepted
-		//$name = $_POST['txtName'];
-        
-if(ctype_alpha($_POST['txtName'])) //Letters A-Z and a-z are accepted
-		$name = $_POST['txtName'];
-	elseif (!ctype_alpha($_POST['txtName']))
-		$name = "type letters a-z only";
+// Sample Scripts
+// Script name: SampleOutput_2.php
+// Output from SampleInput_2.php
 
-	if(ctype_digit($_POST['txtAge'])) // Only integers or whole number is accepted
-		$age = $_POST['txtAge'];
-	elseif(!ctype_digit($_POST['txtAge']))
-		$age = "type numbers 0-9 only";
-		
-	if(empty($_POST['txtPhone'])) // Will test if the textbox is empty value
-		$phone = "phone number is empty";	
-	elseif(is_string($_POST['txtPhone'])) // Will accept string values (any characters) 
-		$phone = $_POST['txtPhone'];
-			
-	if(is_numeric($_POST['txtBill'])) // Only numbers are accepted (integer/floating point)
-		$bill = $_POST['txtBill'];
-	elseif(!is_numeric($_POST['txtBill']))
-		$bill = "letters and special characters are not allowed";
-	
-	echo "OUTPUT WITH INPUT VALIDATION<br><br>";
-	echo "<b>Name:</b> $name <br>";
-	echo "<b>Age:</b> $age <br>";
-	echo "<b>Phone:</b> $phone <br>";
-	echo "<b>Bill:</b> $bill <br>";	
+if(ctype_alpha($_POST['txtName'])) // Letters A-Z and a-z are accepted
+    $name = $_POST['txtName'];
+else
+    $name = "<span class='error'>Type letters A-Z only</span>";
+
+if(ctype_digit($_POST['txtAge'])) // Only integers or whole numbers are accepted
+    $age = $_POST['txtAge'];
+else
+    $age = "<span class='error'>Type numbers 0-9 only</span>";
+
+if(empty($_POST['txtPhone'])) // Will test if the textbox is empty
+    $phone = "<span class='error'>Phone number is empty</span>";
+else
+    $phone = $_POST['txtPhone'];
+
+if(is_numeric($_POST['txtBill'])) // Only numbers are accepted (integer/floating point)
+    $bill = $_POST['txtBill'];
+else
+    $bill = "<span class='error'>Letters and special characters are not allowed</span>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 2 - Output</title>
+    <link rel="stylesheet" href="exercise2-style.css">
+</head>
+<body>
+
+    <div class="output-container">
+        <h2>OUTPUT WITH INPUT VALIDATION</h2>
+        <p><b>Name:</b> <?php echo $name; ?></p>
+        <p><b>Age:</b> <?php echo $age; ?></p>
+        <p><b>Phone:</b> <?php echo $phone; ?></p>
+        <p><b>Bill:</b> <?php echo $bill; ?></p>
+        <a href="SampleInput_2.php" class="back-button">Go Back</a>
+    </div>
+
+</body>
+</html>
